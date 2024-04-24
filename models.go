a49378f1ecd1758535947a349f17873b2fe371b9
arbyys <adam.petricek@centrum.cz>
2024-04-24 22:03:51 +0200

    Method for config load

--- models.go
+++ models.go
@@ -15,3 +15,38 @@ type dispatcher struct {
 	timeMin  int
 	timeMax  int
 }
+
+// config file structure
+type configFile struct {
+	Cars      carConfig      `yaml:"cars"`
+	Stations  stationType    `yaml:"stations"`
+	Registers registerConfig `yaml:"registers"`
+}
+
+type carConfig struct {
+	Count                int `yaml:"count"`
+	ArrivalTimeMin       int `yaml:"arrival_time_min"`
+	ArrivalTimeMax       int `yaml:"arrival_time_max"`
+	SharedQueueLengthMax int `yaml:"shared_queue_length_max"`
+}
+
+type stationConfig struct {
+	Gas      stationType `yaml:"gas"`
+	Diesel   stationType `yaml:"diesel"`
+	LPG      stationType `yaml:"lpg"`
+	Electric stationType `yaml:"electric"`
+}
+
+type stationType struct {
+	Count          int `yaml:"count"`
+	ServeTimeMin   int `yaml:"serve_time_min"`
+	ServeTimeMax   int `yaml:"serve_time_max"`
+	QueueLengthMax int `yaml:"queue_length_max"`
+}
+
+type registerConfig struct {
+	Count          int `yaml:"count"`
+	HandleTimeMin  int `yaml:"handle_time_min"`
+	HandleTimeMax  int `yaml:"handle_time_max"`
+	QueueLengthMax int `yaml:"queue_length_max"`
+}
--- utils.go
+++ utils.go
@@ -3,17 +3,26 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"gopkg.in/yaml.v3"
 )
 
 func printError(message string) {
 	fmt.Printf("\n[!] Following error thrown by the app:\n%s\n", message)
 }
 
-func loadConfig(filePath string) (string, error) {
+func loadConfig(filePath string) (configFile, error) {
+	config := configFile{}
 	buffer, err := os.ReadFile(filePath)
+
 	if err != nil {
-		return "", err
+		return configFile{}, err
 	}
 
-	return "temp", nil
+	err = yaml.Unmarshal(buffer, &config)
+	if err != nil {
+		return configFile{}, err
+	}
+
+	return config, nil
 }
